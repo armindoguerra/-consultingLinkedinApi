404ad9eb07d1b9642de118fc606945cd53c57551
Armindo Guerra <armindogjr@gmail.com>
2018-08-31 09:45:10 -0300

    Update getAccessToken.py

--- getAccessToken.py
+++ getAccessToken.py
@@ -4,26 +4,36 @@ import json
 """
 As informações redirect_uri, client_id e client_secret são obtidas ao criar uma aplicação na plataforma do 
 Linkedin em https://www.linkedin.com/secure/developer?newapp=
-
 A variável code é obtida configurando a autenticação oauth2. https://developer.linkedin.com/docs/oauth2
 """
 
-url = "https://www.linkedin.com/oauth/v2/accessToken"
+class AuthenticationApi(object):
 
-code = "..."
+	def __init__(self):
+        print("init")
+        pass
 
-Headers = {	    
-    "Content-Type":"application/x-www-form-urlencoded"
-}
+	def oauth2(self):
 
-payload = {
-	"grant_type":"authorization_code",
-	"code":code,
-	"redirect_uri":"...",
-	"client_id":"...",
-	"client_secret": "..."
-} 
+		url = "https://www.linkedin.com/oauth/v2/accessToken"
 
-r = requests.post(url, data=json.dumps(payload), headers=Headers)
-json_data = json.loads(r.text)
-print(json_data)
+		code = "..."
+
+		Headers = {	    
+		    "Content-Type":"application/x-www-form-urlencoded"
+		}
+
+		payload = {
+			"grant_type":"authorization_code",
+			"code":code,
+			"redirect_uri":"...",
+			"client_id":"...",
+			"client_secret": "..."
+		} 
+
+		r = requests.post(url, data=json.dumps(payload), headers=Headers)
+		json_data = json.loads(r.text)
+		
+		accessToken = json_data["access_token"]
+
+		return accessToken
